d3973cbb510c4f094a4be857881001b72577ab9c
kamhol1 <kamhol271@gmail.com>
2023-09-24 20:52:14 +0200

    Fix Flyway migration

--- src/main/resources/db/migration/V7__alter_users_table_add_role_field.sql
+++ src/main/resources/db/migration/V7__alter_users_table_add_role_field.sql
@@ -1,3 +1,7 @@
 CREATE TYPE role_enum AS ENUM ('USER', 'ADMIN');
 
 ALTER TABLE users ADD role role_enum DEFAULT 'USER' NOT NULL;
+
+INSERT INTO users (id, first_name, last_name, username, password, role) VALUES
+    (1, 'Admin', 'Admin', 'admin', '$2a$12$OsGKKTdxbd1TVrANHhEfSOP1mo5qm4SBrBPQYhymgEY9GL5m30ONC', 'ADMIN'),
+    (2, 'User', 'User', 'user', '$2a$12$CjiV7yJ0sYXEj7DbBbc5qu6tEjlEApRwXYvElfB23R/9eDuFf9lVK', 'USER');
